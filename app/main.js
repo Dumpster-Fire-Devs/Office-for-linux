5ccccee4178236e06ac5ecbf0034e989895dc9fe
MEGA_Dawg68 <140035326+MEGA-Dawg68@users.noreply.github.com>
2023-11-12 18:12:34 +0800

    V1

---
+++ app/main.js
@@ -0,0 +1,32 @@
+// Import the electron module
+const { app, BrowserWindow } = require('electron')
+
+// Function to create a new BrowserWindow instance
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+  // Load a website into the window
+  win.loadURL('https://www.office.com')
+}
+
+// Create the window when Electron is ready
+app.whenReady().then(createWindow)
+
+// Quit the application when all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
